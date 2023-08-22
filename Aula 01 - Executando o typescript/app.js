818a3eb7796e863a27118306b93c01db4953323c
Thomaz Alves <thomazhilario5@gmail.com>
2023-08-22 11:58:19 -0300

    Executando o typescript e compilando

---
+++ Aula 01 - Executando o typescript/app.js
@@ -0,0 +1,5 @@
+// Executando o typescript
+function sum(a, b) {
+    return a + b;
+}
+console.log("O valor é: ", sum(2, 3));
---
+++ Aula 01 - Executando o typescript/app.ts
@@ -0,0 +1,7 @@
+// Executando o typescript
+
+function sum(a:number,b:number){
+    return a + b
+}
+
+console.log("O valor é: ",sum(2,3))
