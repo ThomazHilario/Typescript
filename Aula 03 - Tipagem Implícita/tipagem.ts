2ca17d908225bf851061de8fed3579504ec3bace
Thomaz Alves <thomazhilario5@gmail.com>
2023-08-23 10:17:00 -0300

    Tipagem implícita

---
+++ Aula 03 - Tipagem Implícita/tipagem.ts
@@ -0,0 +1,8 @@
+/*
+    -> Tipagem implícita
+    - Ao declarar uma variável e não declarar o tipo, e sim só o conteúdo que ela recebe, o typescript entende que a tipagem daquela variável será o tipo de dado recebido pela variável.
+*/
+
+let userName = 'Thomaz'
+
+// Ao declarar a variável userName com dado do tipo string, o typescript entende que a tipagem para aquela variável sera do tipo String.
