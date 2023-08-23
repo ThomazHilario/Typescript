b3e87cc0ec93fc92657b6356352ba779b20b178f
Thomaz Alves <thomazhilario5@gmail.com>
2023-08-23 11:36:02 -0300

    configurando

--- Aula 01 - Executando o typescript/app.js
+++ Aula 01 - Executando o typescript/app.js
@@ -1,4 +1,7 @@
-// Executando o typescript
+/*
+    -> Executando o typescript
+    - Podemos compilar o typescript para javaScript no terminal usando tsc nomedoarquivo.ts
+*/
 function sum(a, b) {
     return a + b;
 }
