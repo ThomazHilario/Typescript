22c22a7bd6b7b3059e547c4d5dbf80842975bcb8
Thomaz Alves <thomazhilario5@gmail.com>
2023-08-23 09:52:58 -0300

    Tipagem explícita

---
+++ Aula 02 - Tipagem Explícita/tipagem.ts
@@ -0,0 +1,17 @@
+/*
+    -> Tipagem explícita
+    - Tem o objetivo de deixar claro o tipo de dado que uma variável ou argumento de função pode receber.
+    - Fazemos o uso de :, e após os dois pontos declaramos o tipo de dado.
+*/
+
+// Para variáveis
+
+let userName:string = 'Thomaz'
+
+// Para funções
+
+function mensagem(mensagem:string){
+    console.log(mensagem)
+}
+
+console.log('Hello World')
