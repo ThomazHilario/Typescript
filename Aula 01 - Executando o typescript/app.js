47755330cf6aa7b7742b1103351ae073c722fd2c
Thomaz Alves <thomazhilario5@gmail.com>
2023-08-23 11:37:22 -0300

    formas de compilar o arquivo.ts

--- Aula 01 - Executando o typescript/app.js
+++ Aula 01 - Executando o typescript/app.js
@@ -1,6 +1,7 @@
 /*
     -> Executando o typescript
     - Podemos compilar o typescript para javaScript no terminal usando tsc nomedoarquivo.ts
+    - Caso queira compilar usando a versão mais recente do ecmascript usamos no terminal: tsc nomedoarquivo.ts --target "ESNEXT" 
 */
 function sum(a, b) {
     return a + b;
