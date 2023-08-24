9c1d23e2bd7b6dc5191861fb6d2b9e6efbe54e14
Thomaz Alves <thomazhilario5@gmail.com>
2023-08-24 11:02:07 -0300

    Tipos primitivos de dados

---
+++ Aula 04 - Tipos de dados/tipos_de_dados.ts
@@ -0,0 +1,16 @@
+/*
+    -> Tipos de dados
+    - O typeScript aceita os tipos primitivos de dados: string, number e boolean.
+*/
+
+// String
+let nameAnime:string = 'Bungou stray dogs'; // Variável com tipagem explícita.
+let nameOtherAnime = "Jujutsu Kaisen"; // Variável com tipagem implícita.
+
+// Number
+let age:number = 18; // Variável com tipagem explícita
+let value = 20; // Variável com tipagem implícita
+
+// Boolean
+let isStudent:boolean = true; // Variável com tipagem explícita
+let isAutenticate = false; // Variável com tipagem implícita
