ba0ecfe2a1af00a27237444803a9814f2a5a6c30
Thomaz Alves <thomazhilario5@gmail.com>
2023-09-04 11:07:43 -0300

    type alias

---
+++ Aula 08 - Type Alias/type_alias.ts
@@ -0,0 +1,9 @@
+/*
+    -> type alias
+    - Uma tipagem criada pelo desenvolvedor onde pode ter varios tipos de dados.
+*/
+
+type list = string[] | number[] | (string | number)[] 
+
+
+let lista:list = []
