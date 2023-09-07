5d57dcad48c856e57d0c9f842401d76eff838c30
Thomaz Alves <thomazhilario5@gmail.com>
2023-09-07 16:59:44 -0300

    encapsulamento:public

---
+++ Aula 09 - POO/encapsulamento.ts
@@ -0,0 +1,13 @@
+/*
+    -> Encapsulamento
+    - Forma de deixar propriedades ou metodos mais protegidos.
+    - As propriedades e métodos podem receber por meio do encapsulamento: public(padrão), private, protected e readonly.
+*/
+
+class Person{
+    // Definindo propriedades
+    id:number;
+    name:string;
+    age:number;
+    public email:string;
+}
