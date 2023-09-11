caae3c5199817afbef3e9cb0aff4bb8819ad6298
Thomaz Alves <thomazhilario5@gmail.com>
2023-09-11 14:14:50 -0300

    POO:Private

--- Aula 09 - POO/encapsulamento.ts
+++ Aula 09 - POO/encapsulamento.ts
@@ -1,20 +1,21 @@
 /*
     -> Encapsulamento
-    - Forma de deixar propriedades ou metodos mais protegidos.
+    - Forma de deixar propriedades ou métodos mais protegidos.
     - As propriedades e métodos podem receber por meio do encapsulamento: public(padrão), private, protected e readonly.
-    - Protected: Atributos e metodos protegidos podem ser alterados ou acessados somente dentro da class ou subclass.
+    - Protected: Atributos e métodos protegidos podem ser alterados ou acessados somente dentro da class ou subclass.
+    - Private: Atributos e métodos só podem ser acessados dentro da classe criada.
 */
 
 class Person{
     // Definindo propriedades
-    id:number;
+    public id:number = 1;
     protected name:string;
     age:number;
     public email:string;
 
     // constructor
-    constructor(id:number, name:string,age:number,email:string){
-        this.id = id;
+    constructor(id:number,name:string,age:number,email:string){
+        this.id = id
         this.name = name;
         this.age = age;
         this.email = email
@@ -23,11 +24,19 @@ class Person{
     nameIs(){
         // Acessando propriedade protected
         console.log(this.name)
+
+        // Mostrando informações
+        thomaz.mostrarInformacoes()
+    }
+
+    // Criando método privado
+    private mostrarInformacoes(){
+        console.log(this.id,this.name,this.age,this.email)
     }
 }
 
 // Instanciando a class
-let thomaz = new Person(2,`Thomaz`,18,`thomaz@email.com`)
+let thomaz = new Person(1,`Thomaz`,18,`thomaz@email.com`)
 
-// acessando uma porpriedade protected por meio de um método.
+// acessando uma propriedade protected por meio de um método.
 thomaz.nameIs()
