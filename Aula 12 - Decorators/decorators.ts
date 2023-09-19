ca706182162e44ea86bbe9c457f35787705482d0
Thomaz Alves <thomazhilario5@gmail.com>
2023-09-19 11:10:58 -0300

    Decorators em typescript

--- Aula 12 - Decorators/decorators.ts
+++ Aula 12 - Decorators/decorators.ts
@@ -1,3 +1,24 @@
 /*
     -> Decorators
+    - Podem ser usados em: classes,métodos,propriedades.
+    - Pode adicionar novos recursos, ou vigiar / verificar alguma coisa.
+    - O decorator tem a estrutura de uma funcao
 */
+
+// Criando a estrutura 
+function logInfo(target: any){
+    console.log('Funcionando')
+}
+
+@logInfo
+class pessoa{}
+
+// O decorator tem o padrao (fabrica) | no qual recebe um parametro e retorna o proprio decorator.
+
+function logClass(menssagem:string){
+    return (target:any) => {
+        console.log(menssagem,target)
+    }
+}
+@logClass('Servidor ligado')
+class sistema{}
