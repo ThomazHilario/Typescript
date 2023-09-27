6a763334659003571e6766618583b5f432ffd94b
Thomaz Alves <thomazhilario5@gmail.com>
2023-09-27 12:23:41 -0300

    Excluindo o script index2.js

--- Praticando/Exercicio - Expanding cards/script/index2.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-let cards = document.querySelectorAll('img');
-cards.forEach((img, idx) => {
-    img.addEventListener('click', function () {
-        console.log('oi');
-    });
-});
